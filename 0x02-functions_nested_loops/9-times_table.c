169f0f3e5727787c423da3864e97d446b0343bd8
Merve322 <njikoumerveille@gmail.com>
2022-03-16 21:42:23 -0700

    Print times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,13 +3,12 @@
  * times_table - 9 times table
  *
  * Return: no return value
- *
  */
 void times_table(void)
 {
 	int i, j, product;
 	for (i = 0; i < 10; i++)
-	{
+	 {
 		for (j = 0; j < 10;  j++)
 		{
 			product = i * j;
